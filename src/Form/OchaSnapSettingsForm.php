6fc252da500bc72b6f4fd4df316764916ac4ba6b
Peter Droogmans (attiks) <peter@attiks.com>
2019-12-11 16:02:52 +0100

    CS

--- src/Form/OchaSnapSettingsForm.php
+++ src/Form/OchaSnapSettingsForm.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\ocha_snap\Controller\OchaSnapController.
- */
-
 namespace Drupal\ocha_snap\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
@@ -44,39 +39,39 @@ class OchaSnapSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config(static::SETTINGS);
 
-    $form['tokens'] = array(
+    $form['tokens'] = [
       '#type'   => 'item',
       '#title'  => $this->t('Tokens'),
       '#markup' => $this->t('You can use the token <em>[pagination]</em> in both the header and footer. It will be replaced with a translated version of the text <em>Page current of total</em> with the appropriate numbers inserted.'),
-    );
+    ];
 
-    $form['header'] = array(
+    $form['header'] = [
       '#type'          => 'textarea',
       '#title'         => $this->t('PDF Header'),
       '#description'   => $this->t('HTML content to use as the generated PDF header. You can <em>not</em> use remote references for images or stylesheets. Any javascript is ignored.'),
       '#default_value' => $config->get('header'),
-    );
+    ];
 
-    $form['footer'] = array(
+    $form['footer'] = [
       '#type'          => 'textarea',
       '#title'         => $this->t('PDF Footer'),
       '#description'   => $this->t('HTML content to use as the generated PDF footer. You can <em>not</em> use remote references for images or stylesheets. Any javascript is ignored.'),
       '#default_value' => $config->get('footer'),
-    );
+    ];
 
-    $form['css'] = array(
+    $form['css'] = [
       '#type'          => 'textarea',
       '#title'         => $this->t('PDF CSS'),
       '#description'   => $this->t('Any custom CSS rules you need injected into the page before the PDF is generated.'),
       '#default_value' => $config->get('css'),
-    );
+    ];
 
-    $form['debug'] = array(
+    $form['debug'] = [
       '#type'          => 'checkbox',
       '#title'         => $this->t('Debug'),
       '#description'   => $this->t('Collect and log debug information in the Snap Service backend.'),
       '#default_value' => $config->get('debug'),
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -94,4 +89,5 @@ class OchaSnapSettingsForm extends ConfigFormBase {
 
     parent::submitForm($form, $form_state);
   }
+
 }
